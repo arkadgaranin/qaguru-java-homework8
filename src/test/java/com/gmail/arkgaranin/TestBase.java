6542bf435d021687022e69fe86c46a7ec1373d5b
Arkadiy <arkadiy.garanin@socialsys.ru>
2022-01-24 23:40:40 +0300

    Создал класс TestBase

---
+++ src/test/java/com/gmail/arkgaranin/TestBase.java
@@ -0,0 +1,12 @@
+package com.gmail.arkgaranin;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+  @BeforeAll
+  static void setup() {
+    Configuration.browserSize = "1920x1080";
+  }
+}
